8d816ef15d2c6e1921709f6e700771d8e084c926
Jon Vaillant <1129497+exquisiteOntologist@users.noreply.github.com>
2023-09-23 15:28:51 +0800

    Simplify implementation

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,29 @@
-use std::fs::{File, OpenOptions};
+use std::fs::OpenOptions;
 use std::io;
-use std::io::prelude::*;
-use std::os::unix::prelude::FileExt;
 use std::path::Path;
-use std::{env, fs, process};
+use std::{env, process};
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
-    if &args.is_empty() == &true {
+    if &args.len() < &2 {
         println!("To perish a file provide a valid file path");
         process::exit(1);
     };
 
     let path = Path::new(args[1].as_str());
-    let path_full = fs::canonicalize(&path).unwrap();
 
-    println!("Perishing file {:?}", &path_full);
-
-    let fo = File::open(&path).unwrap();
-    let f_size = fo.metadata()?.len();
-    let cut_point = f_size / 2;
+    println!("Perishing file {:?}", path.to_str().unwrap());
 
     let f = OpenOptions::new().read(true).write(true).truncate(false).open(&path)?;
+    let f_size = f.metadata()?.len();
 
     // erase the 2nd half of the file
-    f.set_len(cut_point)?;
+    f.set_len(f_size / 2)?;
     // add empty 0s in place of 2nd half
     f.set_len(f_size)?;
 
-    f.sync_all()?;
+    // f.sync_all()?;
    
     println!("file has now perished");
 
